e11bd9a2e3f5bb5cf23901e6c91a4279573d47d2
Ryan Clark <ryan.clark.j@gmail.com>
2011-07-15 15:43:44 -0700

    Added __unicode__ to deliverable list_display instead of just the data_item

--- admin.py
+++ admin.py
@@ -38,7 +38,7 @@ class SubmissionAdmin(admin.ModelAdmin):
     inlines = [CommentInline]
 
 class DeliverableAdmin(admin.ModelAdmin):
-    list_display = ['data_item', 'category', 'is_satisfied']
+    list_display = ['__unicode__', 'category', 'is_satisfied']
     list_filter = ['state']
         
 #admin.site.register(State, StateAdmin)
