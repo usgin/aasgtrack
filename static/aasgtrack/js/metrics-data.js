b17341fa5d40c3e44d0987b5bf9a683668689e04
Ryan Clark <ryan.clark.j@gmail.com>
2011-08-26 12:22:31 -0700

    Removed sortability from the metrics tables.

--- static/aasgtrack/js/metrics-data.js
+++ static/aasgtrack/js/metrics-data.js
@@ -17,13 +17,13 @@ var metricsReader = new Ext.data.JsonReader({
 });
 
 var metricsColumns = [
-    {id: 'state_name', header: 'State', sortable: true, dataIndex: 'state_name', hidden: true},
-    {id: 'category', header: 'Content Category', sortable: true, dataIndex: 'category'},
-    {id: 'deliverableCount', header: 'Expected Deliverables', sortable: true, dataIndex: 'deliverableCount'},
-    {id: 'deliverablesComplete', header: 'Completed Deliverables', sortable: true, dataIndex: 'deliverablesComplete'},
-    {id: 'completion', header: 'Percent Complete', sortable: true, dataIndex: 'completion', renderer: function(value) { response = Ext.util.Format.round(value, 2) + '%'; return response;} },
-    {id: 'onlineCount', header: 'Records Online', sortable: true, dataIndex: 'onlineCount'},
-    {id: 'recentSubmission', header: 'Most Recent Submission', sortable: true, dataIndex: 'recentSubmission', renderer: Ext.util.Format.dateRenderer('M j, Y')}
+    {id: 'state_name', header: 'State', dataIndex: 'state_name', hidden: true, menuDisabled: true},
+    {id: 'category', header: 'Content Category', dataIndex: 'category', menuDisabled: true},
+    {id: 'deliverableCount', header: 'Expected Deliverables', dataIndex: 'deliverableCount', menuDisabled: true},
+    {id: 'deliverablesComplete', header: 'Completed Deliverables', dataIndex: 'deliverablesComplete', menuDisabled: true},
+    {id: 'completion', header: 'Percent Complete', dataIndex: 'completion', renderer: function(value) { response = Ext.util.Format.round(value, 2) + '%'; return response;} , menuDisabled: true},
+    {id: 'onlineCount', header: 'Records Online', dataIndex: 'onlineCount', menuDisabled: true},
+    {id: 'recentSubmission', header: 'Most Recent Submission', dataIndex: 'recentSubmission', renderer: Ext.util.Format.dateRenderer('M j, Y'), menuDisabled: true}
 ];
 
 var metricsGroupView = new Ext.grid.GroupingView({
