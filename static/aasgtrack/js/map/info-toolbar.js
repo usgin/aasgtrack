fd67bece67f828c1a2422d8d07bec3be3d9e423d
Ryan Clark <ryan.clark.j@gmail.com>
2011-09-09 13:22:49 -0700

    Fix a width issue in IE9

--- static/aasgtrack/js/map/info-toolbar.js
+++ static/aasgtrack/js/map/info-toolbar.js
@@ -15,8 +15,7 @@ var tutorialToolbar = new Ext.Toolbar({
         		items: [
     		        {
     		        	xtype: 'panel',
-    		        	html: "Data being provided by each state has been categorized into eight different themes, as indicated by the legend near the bottom of the map. Clicking on one of the colored dots on the legend will cause many of the states to become colored. The range of colors that appear indicate how far along each state is in the completion of their data deliverables in the theme that was selected. For a broad report summarizing the data collection across all eight themes and all 50 states, <strong><a href='http://services.usgin.org/track/report/'>click here</a></strong>." ,
-    		        	width: 400,
+    		        	html: "Data being provided by each state has been categorized into eight different themes, as indicated by the legend near the bottom of the map. Clicking on one of the colored dots on the legend will cause many of the states to become colored. The range of colors that appear indicate how far along each state is in the completion of their data deliverables in the theme that was selected. For a broad report summarizing the data collection across all eight themes and all 50 states, <strong><a href='http://services.usgin.org/track/report/'>click here</a></strong>." ,    		        	
     		        	cls: 'map-info-text',
     		        	bodyStyle: panelStyle
     		        }
@@ -32,11 +31,11 @@ var tutorialToolbar = new Ext.Toolbar({
     		        {
     		        	xtype: 'panel',
     		        	html: "Over each state, a colored dot indicates that the state intends to provide data falling under that particular theme, while a grey dot means no data in that theme is currently anticipated. By clicking one of the colored dots on a state, you can view the specific deliverables expected from that state in the selected theme, and the progress of those deliverables.",
-    		        	width: 400,
     		        	cls: 'map-info-text',
     		        	bodyStyle: panelStyle
     		        }
-		        ]
+		        ],
+		        plain: true
         	}
 		}, " ", "|", " ",
         { 
@@ -47,11 +46,11 @@ var tutorialToolbar = new Ext.Toolbar({
     		        {
     		        	xtype: 'panel',
     		        	html: "To see more detailed information about the deliverables from any one state, including links to data and web services available, follow the hyperlink that appears at the top of the state's popup box or use the drop-down menu below the map to view a detailed report.",
-    		        	width: 400,
     		        	cls: 'map-info-text',
     		        	bodyStyle: panelStyle
     		        }
-		        ]
+		        ],
+		        plain: true
         	}
 		}
     ]
