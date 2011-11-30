47a051e99aeea8f17567a361e5fe20bef128d341
Ryan Clark <ryan.clark.j@gmail.com>
2011-11-30 09:34:41 -0700

    Adjusted to use ArcGIS Server background coming from Kyanite instead of AWS VM.

--- static/aasgtrack/js/map/ags-layers.js
+++ static/aasgtrack/js/map/ags-layers.js
@@ -1,7 +1,7 @@
 function agsBackgroundLayer(name) {
 	layer = new OpenLayers.Layer.XYZ(
 			name,
-			"http://50.19.88.63/ArcGIS/rest/services/WorldBackground/MapServer/tile/${z}/${y}/${x}",
+			"http://services.azgs.az.gov/ArcGIS/rest/services/baselayers/WorldBackground/MapServer/tile/${z}/${y}/${x}",
 			{ 
 				isBaseLayer: true, 
 				sphericalMercator: true,
@@ -16,7 +16,7 @@ function agsBackgroundLayer(name) {
 function ags93BackgroundLayer(name) {
 	layer = new OpenLayers.Layer.ArcGIS93Rest(
 		name,
-		"http://50.19.88.63/ArcGIS/rest/services/WorldBackground/MapServer/export",
+		"http://services.azgs.az.gov/ArcGIS/rest/services/baselayers/WorldBackground/MapServer/export",
 		{
 			layers: "0,1,2"			
 		},
@@ -31,7 +31,7 @@ function ags93BackgroundLayer(name) {
 function agsCacheBackgroundLayer(name) {
 	layer = new OpenLayers.Layer.ArcGISCache(
 			name,
-			"http://50.19.88.63/ArcGIS/rest/services/WorldBackground/MapServer"
+			"http://services.azgs.az.gov/ArcGIS/rest/services/baselayers/WorldBackground/MapServer"
 	);
 	
 	return layer;
